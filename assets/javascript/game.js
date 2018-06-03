afbdb86f3d8bd7f74880fe5556b0708225872776
Lysandra <38792939+Lysandra@users.noreply.github.com>
2018-06-02 21:13:24 -0400

    Corrected Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
     var topFive = ["Javascript", "JQuery", "CSS", "HTML","Fun"];
     var wins = 0;
     var loses = 0;
